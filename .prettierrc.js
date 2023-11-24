9f388a4542b7174f2834c08dc29bd5f9d72717a9
Zhanghao <yukiniro@hotmail.com>
2023-11-24 14:21:43 +0800

    feat: add components

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,12 +2,12 @@ module.exports = {
     printWidth: 120,
     tabWidth: 2,
     useTabs: false,
-    semi: false,
-    singleQuote: false, // 使用单引号
-    trailingComma: 'all', // 尾随逗号
-    bracketSpacing: true, // 对象字面量的花括号间加空格
-    arrowParens: 'avoid', // (x) => {} 是否要有小括号
-    requirePragma: false, // 是否要注释来决定是否格式化代码
-    proseWrap: 'preserve', // 是否要换行
-    endOfLine: 'auto', // 结尾是 \n \r \n\r auto
+    semi: true,
+    singleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    requirePragma: false,
+    proseWrap: 'preserve',
+    endOfLine: 'lf',
 }
