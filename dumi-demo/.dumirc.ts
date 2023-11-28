a85aa6aa0dddf0b19b73d8b8d7e6c071066413c1
Zhanghao <yukiniro@hotmail.com>
2023-11-28 18:51:34 +0800

    chore: update config

--- dumi-demo/.dumirc.ts
+++ dumi-demo/.dumirc.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'dumi-component-demo',
+  outputPath: 'docs-dist',
   themeConfig: {
     name: '组件库',
-  },
-  apiParser: {},
-  resolve: {
-    // 配置入口文件路径，API 解析将从这里开始
-    entryFile: './src/index.tsx',
   },
 });
