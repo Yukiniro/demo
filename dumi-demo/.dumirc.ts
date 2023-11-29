1a5670d66165e272583bc1708dcdfed7c5085d46
Zhanghao <yukiniro@hotmail.com>
2023-11-29 10:56:14 +0800

    chore: update .dumirc.ts

--- dumi-demo/.dumirc.ts
+++ dumi-demo/.dumirc.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'dumi';
+import { defineConfig } from "dumi";
+
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: "dumi-static",
   themeConfig: {
-    name: '组件库',
+    name: "组件库",
   },
+  base: isProd ? "/dumi-static" : "/",
+  publicPath: isProd ? "/dumi-static/" : "/",
 });
