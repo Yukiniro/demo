a4f5cff6872f1700ad16050c221912e19817d475
Yukiniro <yukiniro@hotmail.com>
2024-04-10 22:28:56 +0800

    feat: update

--- redux-demo/src/App.tsx
+++ redux-demo/src/App.tsx
@@ -1,19 +1,28 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./vite.svg";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const count = useSelector((state: { count: number }) => state.count);
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
   return (
     <div className="container mx-auto h-screen flex flex-col justify-center items-center">
-      <h1 className="text-6xl pb-32">Vite + React + Daisyui</h1>
-      <label className="swap swap-flip">
-        <input type="checkbox" />
-        <div className="swap-on">
-          <img src={viteLogo} className="w-36 h-36" alt="Vite logo" />
-        </div>
-        <div className="swap-off">
-          <img src={reactLogo} className="w-36 h-36" alt="React logo" />
-        </div>
-      </label>
+      <h1 className="text-6xl pb-32">Redux</h1>
+      <p>count: {count}</p>
+      <div>
+        <button className="btn" onClick={handleDecrement}>
+          - 1
+        </button>
+        <button className="btn" onClick={handleIncrement}>
+          + 1
+        </button>
+      </div>
     </div>
   );
 }
--- redux-demo/src/main.tsx
+++ redux-demo/src/main.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+// eslint-disable-next-line camelcase
+import { legacy_createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+
+const rootReducer = (state = { count: 0 }, action: { type: string; [key: string]: unknown }) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
