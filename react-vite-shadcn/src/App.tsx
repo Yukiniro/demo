5ca6b1e03231b4f344a03ebf30c2153a29796f78
Zhanghao <yukiniro@hotmail.com>
2024-07-08 11:58:08 +0800

    feat: add react-vite-shadcn

---
+++ react-vite-shadcn/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center flex-col w-full h-full">
+      <h1 className="text-6xl mb-32">React + Vite + Shadcn</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
