4d6af61929da80dcb73a731c27dcb2c0c0d96050
Zhanghao <yukiniro@hotmail.com>
2024-07-10 11:50:10 +0800

    feat: update link

--- open-window-demo/src/App.tsx
+++ open-window-demo/src/App.tsx
@@ -1,25 +1,21 @@
 import { Button } from "@/components/ui/button";
-
-const fn = () => {
-  window.open(
-    "https://www.baidu.com",
-    "_blank",
-    "height=600,width=600,status=yes,top=200,left=400,toolbar=no,menubar=no,location=no",
-  );
-};
+import { useState } from "react";
 
 function App() {
+  const [link, setLink] = useState<string>("https://www.baidu.com");
+
   const handleClick = () => {
-    fn();
+    window.open(link, "_blank", "height=600,width=600,status=yes,top=200,left=400,toolbar=no,menubar=no,location=no");
   };
   const handleClickAsync = () => {
     setTimeout(() => {
-      fn();
+      window.open(link, "_blank", "height=600,width=600,status=yes,top=200,left=400,toolbar=no,menubar=no,location=no");
     }, 1000);
   };
   return (
     <div className="flex items-center justify-center flex-col w-full h-full">
       <h1 className="text-6xl mb-32">React + Vite + Shadcn</h1>
+      <input value={link} onChange={e => setLink(e.target.value)} />
       <div>
         <Button className="m-2" onClick={handleClick}>
           Click Me
