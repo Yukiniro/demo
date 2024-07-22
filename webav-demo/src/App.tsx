59144f37ca6119d13224cc694284ea6e547bc18e
Zhanghao <yukiniro@hotmail.com>
2024-07-22 18:17:38 +0800

    feat: daily update

--- webav-demo/src/App.tsx
+++ webav-demo/src/App.tsx
@@ -1,54 +1,28 @@
-import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
-import videoUrl from "@/assets/video.mp4";
-import audioUrl from "@/assets/audio.mp3";
-import { saveAs } from "file-saver";
-import { MP4Clip, AudioClip, OffscreenSprite, Combinator } from "@webav/av-cliper";
 import { useState } from "react";
+import { SceneRadioGroup } from "./components/scene-radio-group";
+import Combinate from "./components/combinate";
+import PlayVideo from "./components/play-video";
+
+const animations = [
+  {
+    value: "play-video",
+    label: "Play Video",
+  },
+  {
+    value: "combine-audio-video",
+    label: "Combine AV",
+  },
+];
 
 function App() {
-  const [progress, setProgress] = useState(0);
-  const [isPending, setIsPending] = useState(false);
-
-  const handleClick = async () => {
-    if (isPending) return;
-    setIsPending(true);
-    setProgress(0);
-    const videoRes = await fetch(videoUrl);
-    const audioRes = await fetch(audioUrl);
-    const videoClip = new MP4Clip(videoRes.body!);
-    const audioClip = new AudioClip(audioRes.body!);
-    const videoSprite = new OffscreenSprite(videoClip);
-    const audioSprite = new OffscreenSprite(audioClip);
-    await Promise.all([videoSprite.ready, audioSprite.ready]);
-
-    const width = 1280;
-    const height = 720;
-
-    videoSprite.rect.w = width;
-    videoSprite.rect.h = height;
-
-    const com = new Combinator({ width, height });
-    await com.addSprite(videoSprite, { main: true });
-    await com.addSprite(audioSprite);
-
-    com.on("OutputProgress", progress => setProgress(progress));
-    com.on("error", err => alert(err.message));
-
-    const outStream = com.output();
-    const outBlob = await new Response(outStream).blob();
-    saveAs(outBlob, "output.mp4");
-    setIsPending(false);
-  };
-
+  const [animation, setAnimation] = useState(animations[1].value);
   return (
     <div className="flex items-center justify-center flex-col w-full h-full">
-      <h1 className="text-6xl mb-32">WebAV</h1>
-      <Button disabled={isPending} onClick={handleClick}>
-        Click me
-      </Button>
-      <div className="w-72 mt-6">
-        <Progress value={Math.round(progress * 100)} />
+      <h1 className="text-6xl py-16  w-full text-center font-mono font-bold">WeAV Demo</h1>
+      <SceneRadioGroup list={animations} value={animation} onChange={v => setAnimation(v)} />
+      <div className="border-t border-black w-full flex" style={{ height: "calc(100vh - 156px)" }}>
+        {animation === "play-video" && <PlayVideo />}
+        {animation === "combine-audio-video" && <Combinate />}
       </div>
     </div>
   );
---
+++ webav-demo/src/components/play-video.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import videoUrl from "@/assets/video.mp4";
+import audioUrl from "@/assets/audio.mp3";
+import { saveAs } from "file-saver";
+import { MP4Clip, AudioClip, OffscreenSprite, Combinator } from "@webav/av-cliper";
+import { useState } from "react";
+
+function PlayVideo() {
+  const [progress, setProgress] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const handleClick = async () => {
+    if (isPending) return;
+    setIsPending(true);
+    setProgress(0);
+    const videoRes = await fetch(videoUrl);
+    const audioRes = await fetch(audioUrl);
+    const videoClip = new MP4Clip(videoRes.body!);
+    const audioClip = new AudioClip(audioRes.body!);
+    const videoSprite = new OffscreenSprite(videoClip);
+    const audioSprite = new OffscreenSprite(audioClip);
+    await Promise.all([videoSprite.ready, audioSprite.ready]);
+
+    const width = 1280;
+    const height = 720;
+
+    videoSprite.rect.w = width;
+    videoSprite.rect.h = height;
+
+    const com = new Combinator({ width, height });
+    await com.addSprite(videoSprite, { main: true });
+    await com.addSprite(audioSprite);
+
+    com.on("OutputProgress", progress => setProgress(progress));
+    com.on("error", err => alert(err.message));
+
+    const outStream = com.output();
+    const outBlob = await new Response(outStream).blob();
+    saveAs(outBlob, "output.mp4");
+    setIsPending(false);
+  };
+
+  return (
+    <div className="flex items-center justify-center flex-col w-full h-full">
+      <Button disabled={isPending} onClick={handleClick}>
+        Click me
+      </Button>
+      <div className="w-72 mt-6">
+        <Progress value={Math.round(progress * 100)} />
+      </div>
+    </div>
+  );
+}
+
+export default PlayVideo;
