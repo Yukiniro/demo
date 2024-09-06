4c1b692c16e07402ccc0d729db1f10b5441bc8cb
Zhanghao <yukiniro@hotmail.com>
2024-09-06 15:51:04 +0800

    feat: update fade 3d

---
+++ fake-3d-webgl/src/gl/immersity.ts
@@ -0,0 +1,202 @@
+/**
+ * 参考 https://app.immersity.ai/ 的 shader
+ */
+
+export const vertexShaderSource3 = `
+precision highp float;
+#define SHADER_NAME pixi-shader-3
+attribute vec2 aVertexPosition;
+
+uniform mat3 projectionMatrix;
+uniform mat3 filterMatrix;
+
+varying vec2 vTextureCoord;
+varying vec2 vFilterCoord;
+uniform bool isStereoscopic;
+uniform bool isAnaglyph;
+uniform bool isPickingDepth;
+uniform bool isSbsHalfWidth;
+uniform vec4 inputSize;
+uniform vec4 outputFrame;
+
+vec4 filterVertexPosition(void ) {
+  vec2 position = aVertexPosition * max(outputFrame.zw, vec2(0.0)) + outputFrame.xy;
+  if (!isPickingDepth && isStereoscopic && !isAnaglyph && !isSbsHalfWidth) {
+    position =
+      aVertexPosition * max(vec2(outputFrame.z * 2.0, outputFrame.w), vec2(0.0)) +
+      outputFrame.xy;
+  }
+
+  return vec4((projectionMatrix * vec3(position, 1.0)).xy, 0.0, 1.0);
+}
+
+vec2 filterTextureCoord(void ) {
+  return aVertexPosition * (outputFrame.zw * inputSize.zw);
+}
+
+void main(void ) {
+  gl_Position = filterVertexPosition();
+  vTextureCoord = filterTextureCoord();
+  
+  vTextureCoord = aVertexPosition;
+  vFilterCoord = (filterMatrix * vec3(vTextureCoord, 1.0)).xy;
+}
+`;
+
+export const fragmentShaderSource3 = `
+precision mediump float;
+#define SHADER_NAME pixi-shader-3
+precision highp float;
+
+varying vec2 vFilterCoord;
+varying vec2 vTextureCoord;
+uniform sampler2D mapSampler;
+
+uniform sampler2D imageSampler;
+uniform sampler2D watermark;
+uniform vec4 inputSize;
+uniform float scale;
+uniform vec3 offset;
+uniform float focus;
+uniform float enlarge;
+uniform float aspect;
+uniform bool watermarkEnabled;
+uniform bool isPickingDepth;
+uniform bool isStereoscopic;
+uniform bool isAnaglyph;
+uniform bool isSbs;
+uniform bool isSbsHalfWidth;
+uniform bool isCrossEyed;
+uniform bool isLif;
+uniform float stereoGain;
+
+vec3 perspective(
+  vec2 uv,
+  vec3 cameraShift,
+  float convergence 
+) {
+  vec3 ray_origin = vec3(uv - 0.5, 0) * (1.0 - convergence * cameraShift.z); 
+  vec3 ray_direction = vec3(0, 0, 1); 
+
+  
+  ray_origin.xy -= cameraShift.xy * convergence;
+  ray_direction.xy += (uv - 0.5) * cameraShift.z + cameraShift.xy;
+
+  const int step_count = 45; 
+  const float hit_threshold = 0.01;
+  ray_direction /= float(step_count);
+
+  
+  
+  vec3 color = vec3(0.0);
+
+  for (int i = 0; i < step_count; i++) {
+    ray_origin += ray_direction;
+    float scene_z = 1.0 - texture2D(mapSampler, ray_origin.xy + 0.5).x;
+    if (ray_origin.z > scene_z) {
+      if (ray_origin.z - scene_z < hit_threshold) {
+        break;
+      }
+      ray_origin -= ray_direction; 
+      ray_direction /= 2.0; 
+    }
+  }
+
+  color = texture2D(imageSampler, ray_origin.xy + 0.5).rgb;
+  #ifdef DEBUG_CROP
+  if (
+    ray_origin.x < -0.5 ||
+    ray_origin.y < -0.5 ||
+    ray_origin.x >= +0.5 ||
+    ray_origin.y >= +0.5
+  ) {
+    color.r = 1.0;
+  }
+  #endif
+  return color;
+}
+
+void main(void ) {
+  vec2 uv =
+    (vTextureCoord - vec2(0.5)) /
+      vec2(!isPickingDepth && isStereoscopic ? 1.0 : enlarge) +
+    vec2(0.5);
+  vec2 uvSbs = vTextureCoord;
+  if (!isPickingDepth && isStereoscopic) {
+    if (isAnaglyph) {
+      vec4 left = vec4(perspective(uv, vec3(stereoGain, 0.0, 0.0), focus), 1.0);
+      vec4 right = vec4(perspective(uv, vec3(-stereoGain, 0.0, 0.0), focus), 1.0);
+      const vec4 left_glass = vec4(1, 0, 0, 1);
+      const vec4 right_glass = vec4(0, 1, 1, 1);
+      gl_FragColor = left * left_glass + right * right_glass;
+    } else if (isSbs || isCrossEyed || isSbsHalfWidth) {
+      
+      if (uvSbs.y < 0.0 || uvSbs.y >= 1.0) {
+        gl_FragColor = vec4(0);
+        return;
+      }
+
+      if (uvSbs.x < 0.5) {
+        uvSbs.x *= 2.0;
+        vec4 left = vec4(
+          perspective(
+            uvSbs,
+            vec3(stereoGain * (isCrossEyed ? -1.0 : 1.0), 0.0, 0.0),
+            focus
+          ),
+          1.0
+        );
+        gl_FragColor = left;
+      } else {
+        uvSbs.x -= 0.5;
+        uvSbs.x *= 2.0;
+        vec4 right = vec4(
+          perspective(
+            uvSbs,
+            vec3(stereoGain * (isCrossEyed ? 1.0 : -1.0), 0.0, 0.0),
+            focus
+          ),
+          1.0
+        );
+        gl_FragColor = right;
+      }
+    } else if (isLif) {
+      if (uvSbs.y < 0.0 || uvSbs.y >= 1.0) {
+        gl_FragColor = vec4(0);
+        return;
+      }
+
+      if (uvSbs.x < 0.5) {
+        uvSbs.x *= 2.0;
+        vec4 left = texture2D(imageSampler, uvSbs);
+        gl_FragColor = left;
+      } else {
+        uvSbs.x -= 0.5;
+        uvSbs.x *= 2.0;
+        vec4 right = texture2D(mapSampler, uvSbs);
+        gl_FragColor = right;
+      }
+    }
+  } else {
+    gl_FragColor = vec4(
+      perspective(uv, vec3(offset.x, offset.y * aspect, offset.z), focus),
+      1.0
+    );
+  }
+
+  if (watermarkEnabled) {
+    bool isSbs = isStereoscopic && !isAnaglyph;
+    vec2 wmuv = isSbs ? uvSbs : vTextureCoord;
+    if (aspect > 1.0) {
+      wmuv.y -= 0.5;
+      wmuv.y /= aspect;
+      wmuv.y += 0.5;
+    } else {
+      wmuv.x -= 0.5;
+      wmuv.x *= aspect;
+      wmuv.x += 0.5;
+    }
+    gl_FragColor += texture2D(watermark, wmuv);
+  }
+}
+`;
--- fake-3d-webgl/src/gl/index.ts
+++ fake-3d-webgl/src/gl/index.ts
@@ -9,21 +9,49 @@ void main() {
 
 export const fragmentShaderSource = `#version 300 es
 precision highp float;
-uniform vec2 u_resolution;
+
 in vec2 v_texCoord;
+
+uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform sampler2D image0;
 uniform sampler2D image1;
+
 out vec4 outColor;
 
-vec2 mirrored(vec2 v) {
-  vec2 m = mod(v,2.);
-  return mix(m,2.0 - m, step(1.0 ,m));
+vec3 perspective(
+  vec2 uv,
+  vec3 cameraShift,
+  float convergence 
+) {
+  vec3 ray_origin = vec3(uv - 0.5, 0) * (1.0 - convergence * cameraShift.z); 
+  vec3 ray_direction = vec3(0, 0, 1); 
+
+  ray_origin.xy -= cameraShift.xy * convergence;
+  ray_direction.xy += (uv - 0.5) * cameraShift.z + cameraShift.xy;
+
+  const int step_count = 45; 
+  const float hit_threshold = 0.01;
+  ray_direction /= float(step_count);
+
+  vec3 color = vec3(0.0);
+
+  for (int i = 0; i < step_count; i++) {
+    ray_origin += ray_direction;
+    float scene_z = 1.0 - texture(image1, ray_origin.xy + 0.5).x;
+    if (ray_origin.z > scene_z) {
+      if (ray_origin.z - scene_z < hit_threshold) {
+        break;
+      }
+      ray_origin -= ray_direction; 
+      ray_direction /= 2.0; 
+    }
+  }
+
+  return texture(image0, ray_origin.xy + 0.5).rgb;
 }
 
-void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  uv.y = 1. - uv.y;
+void main(void) {
 
   float focus = 0.5;
   float scaleX = 50.0;
@@ -31,228 +59,8 @@ void main() {
   float x_offset = u_mouse.x / u_resolution.x * scaleX;
   float y_offset = u_mouse.y / u_resolution.y * scaleY;
 
-  float depth = texture(image1, mirrored(uv)).r;
-
-  vec2 fakeUV = uv - (depth - focus) * vec2(x_offset, y_offset);
-
-  outColor = texture(image0, mirrored(fakeUV));
-}`;
-
-export const fragmentShaderSource0 = `#version 300 es
-precision highp float;
-uniform vec2 u_resolution;
-in vec2 v_texCoord;
-uniform vec2 u_mouse;
-uniform sampler2D image0;
-uniform sampler2D image1;
-out vec4 outColor;
-
-vec2 mirrored(vec2 v) {
-  vec2 m = mod(v,2.);
-  return mix(m,2.0 - m, step(1.0 ,m));
-}
-
-void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution.xy;
   uv.y = 1. - uv.y;
 
-  float focus = 0.5;
-  float scaleX = 50.0;
-  float scaleY = 30.0;
-  float x_offset = u_mouse.x / u_resolution.x * scaleX;
-  float y_offset = u_mouse.y / u_resolution.y * scaleY;
-
-  float depth = texture(image1, mirrored(uv)).r;
-
-  vec2 fakeUV = uv - (depth - focus) * vec2(x_offset, y_offset);
-
-  outColor = texture(image0, mirrored(fakeUV));
-}`;
-
-export const fragmentShaderSource3 = `#version 300 es
-precision highp float;
-uniform vec2 u_resolution;
-in vec2 v_texCoord;
-uniform vec2 u_mouse;
-uniform sampler2D image0;
-uniform sampler2D image1;
-out vec4 outColor;
-
-vec2 mirrored(vec2 v) {
-  vec2 m = mod(v,2.);
-  return mix(m,2.0 - m, step(1.0 ,m));
-}
-
-bool isDepthDiscontinuity(sampler2D depthMap, vec2 uv, float threshold) {
-  float center = texture(depthMap, uv).r;
-  float left = texture(depthMap, uv - vec2(1.0 / u_resolution.x, 0.0)).r;
-  float right = texture(depthMap, uv + vec2(1.0 / u_resolution.x, 0.0)).r;
-  float top = texture(depthMap, uv - vec2(0.0, 1.0 / u_resolution.y)).r;
-  float bottom = texture(depthMap, uv + vec2(0.0, 1.0 / u_resolution.y)).r;
-  
-  return abs(center - left) > threshold ||
-         abs(center - right) > threshold ||
-         abs(center - top) > threshold ||
-         abs(center - bottom) > threshold;
-}
-
-float getSmoothedDepth(sampler2D depthMap, vec2 uv, float radius) {
-  vec2 texelSize = 1.0 / vec2(textureSize(depthMap, 0));
-  float result = 0.0;
-  float weightSum = 0.0;
-
-  for(float x = -radius; x <= radius; x += 1.0) {
-    for(float y = -radius; y <= radius; y += 1.0) {
-      vec2 offset = vec2(x, y) * texelSize;
-      float weight = 1.0 - length(vec2(x, y)) / (radius * 1.4142);
-      weight = max(0.0, weight);
-      result += texture(depthMap, uv + offset).r * weight;
-      weightSum += weight;
-    }
-  }
-
-  return result / weightSum;
-}
-
-vec4 boxBlur(sampler2D tex, vec2 uv, vec2 resolution, float radius) {
-    vec4 color = vec4(0.0);
-    float total = 0.0;
-    for (float x = -radius; x <= radius; x += 1.0) {
-        for (float y = -radius; y <= radius; y += 1.0) {
-            vec2 offset = vec2(x, y) / resolution;
-            color += texture(tex, uv + offset);
-            total += 1.0;
-        }
-    }
-    return color / total;
-}
-
-void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  uv.y = 1. - uv.y;
-
-  float focus = 0.5;
-  float scaleX = 50.0;
-  float scaleY = 30.0;
-  float x_offset = u_mouse.x / u_resolution.x * scaleX;
-  float y_offset = u_mouse.y / u_resolution.y * scaleY;
-
-  float depth = texture(image1, mirrored(uv)).r;
-
-  vec2 fakeUV = uv - (depth - focus) * vec2(x_offset, y_offset);
-
-  float depthSampleDepth = texture(image1, mirrored(fakeUV)).r;
-
-  if (depth < focus && depthSampleDepth > focus) {
-    // outColor = texture(image0, mirrored(fakeUV));
-    // outColor.rgb = vec3(1.0, 0.0, 0.0);
-
-    outColor = boxBlur(image0, mirrored(uv), u_resolution, 10.0);
-  } else {
-    outColor = texture(image0, mirrored(fakeUV));
-  }
-
-}`;
-
-export const fragmentShaderSource2 = `#version 300 es
-precision highp float;
-uniform vec2 u_resolution;
-in vec2 v_texCoord;
-uniform vec2 u_mouse;
-uniform sampler2D image0;
-uniform sampler2D image1;
-out vec4 outColor;
-
-vec2 mirrored(vec2 v) {
-  vec2 m = mod(v,2.);
-  return mix(m,2.0 - m, step(1.0 ,m));
-}
-
-void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  uv.y = 1. - uv.y;
-
-  float focus = 0.5;
-  float scaleX = 50.0;
-  float scaleY = 30.0;
-  float x_offset = -u_mouse.x / u_resolution.x * scaleX;
-  float y_offset = -u_mouse.y / u_resolution.y * scaleY;
-
-  float depth = texture(image1, mirrored(uv)).r;
-
-  vec2 planeUV = uv - vec2(x_offset, y_offset);
-  float planeDepth = texture(image1, mirrored(planeUV)).r;
-
-  vec2 effectUV = uv - (depth - focus) * vec2(x_offset, y_offset);
-  float effectDepth = texture(image1, mirrored(effectUV)).r;
-
-  float depthOffset = depth - focus;
-
-  float mixDepth = depth;
-  if (planeDepth > focus) {
-    depthOffset = planeDepth - focus;
-  } else {
-
-    depthOffset = depth - focus;
-
-    if (depth > focus) {
-      outColor = vec4(0.0, effectDepth, 0.0, 1.0);
-      return;
-    } else {
-     depthOffset = 0.2;
-      // outColor = vec4(planeDepth - focus, 0.0, 0.0, 1.0);
-      // return;
-    }
-
-    // outColor = vec4(1.0, 1.0, 0.0, 1.0);
-    // return;
-  }
-
-  vec2 fakeUV = uv - depthOffset * vec2(x_offset, y_offset);
-
-  outColor = texture(image0, mirrored(fakeUV));
-}`;
-
-export const fragmentShaderSource1 = `#version 300 es
-precision highp float;
-uniform vec2 u_resolution;
-in vec2 v_texCoord;
-uniform sampler2D u_texture;
-uniform vec2 u_mouse;
-uniform vec2 u_threshold;
-uniform sampler2D image0;
-uniform sampler2D image1;
-out vec4 outColor;
-
-float getSmoothedDepth(sampler2D depthMap, vec2 uv, float radius) {
-  vec2 texelSize = 1.0 / vec2(textureSize(depthMap, 0));
-  float result = 0.0;
-  float weightSum = 0.0;
-
-  for(float x = -radius; x <= radius; x += 1.0) {
-    for(float y = -radius; y <= radius; y += 1.0) {
-      vec2 offset = vec2(x, y) * texelSize;
-      float weight = 1.0 - length(vec2(x, y)) / (radius * 1.4142);
-      weight = max(0.0, weight);
-      result += texture(depthMap, uv + offset).r * weight;
-      weightSum += weight;
-    }
-  }
-
-  return result / weightSum;
-}
-
-void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  uv.y = 1. - uv.y;
-
-  float focus = 0.5;
-  float scaleX = 25.0;
-  float scaleY = 15.0;
-  float depth = getSmoothedDepth(image1, uv, 25.0);
-  float x_offset = u_mouse.x / u_resolution.x * scaleX;
-  float y_offset = u_mouse.y / u_resolution.y * scaleY;
-  vec2 fake3d = uv + (depth - focus) * vec2(x_offset, y_offset);
-
-  outColor = texture(image0, fake3d);
+  outColor = vec4(perspective(uv, vec3(x_offset, y_offset, 0.0), focus), 1.0);
 }`;
