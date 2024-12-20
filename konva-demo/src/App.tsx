2a300f78f3c8649eea6fada6cfaffe48900e9980
Zhanghao <yukiniro@hotmail.com>
2024-12-20 18:07:20 +0800

    feat: update konva demo

--- konva-demo/src/App.tsx
+++ konva-demo/src/App.tsx
@@ -2,45 +2,70 @@ import { Button } from "@/components/ui/button";
 import Konva from "konva";
 import { useRef, useEffect } from "react";
 
+// const enabledAnchors = ["top-left", "top-right", "bottom-left", "bottom-right"];
+
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const appRef = useRef<Konva.Stage | null>(null);
+  const layerRef = useRef<Konva.Layer | null>(null);
+  // const transformerRef = useRef<Konva.Transformer | null>(null);
 
   useEffect(() => {
     if (!containerRef.current) {
       return;
     }
     const { width, height } = containerRef.current.getBoundingClientRect();
-    const stage = new Konva.Stage({
+    appRef.current = new Konva.Stage({
       container: containerRef.current,
       width,
       height,
     });
+    const stage = appRef.current;
+    if (!stage) {
+      return;
+    }
 
-    const layer = new Konva.Layer({ draggable: true });
+    const layer = new Konva.Layer();
+    layerRef.current = layer;
 
-    const rect = new Konva.Rect({
-      x: 100,
-      y: 500,
-      width: 500,
-      height: 500,
-      fill: "blue",
-    });
-
-    const transformer = new Konva.Transformer({
-      nodes: [rect],
-      rotateAnchorOffset: 60,
-      enabledAnchors: ["top-left", "top-right", "bottom-left", "bottom-right"],
-    });
-    layer.add(transformer);
-
-    layer.add(rect);
     stage.add(layer);
   }, []);
 
+  const handleNewRect = () => {
+    const layer = layerRef.current;
+    if (!layer) {
+      return;
+    }
+
+    const rect = new Konva.Rect({
+      x: Math.floor(Math.random() * 401) + 100,
+      y: Math.floor(Math.random() * 401) + 100,
+      width: Math.floor(Math.random() * 401) + 100,
+      height: Math.floor(Math.random() * 401) + 100,
+      fill: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+      draggable: true,
+    });
+
+    layer.add(rect);
+
+    // transformerRef.current =
+    //   transformerRef.current ||
+    //   new Konva.Transformer({
+    //     nodes: [rect],
+    //     rotateAnchorOffset: 60,
+    //     enabledAnchors: enabledAnchors,
+    //   });
+
+    // transformerRef.current.setNodes([rect]);
+    // layer.add(transformerRef.current);
+  };
+
   return (
-    <div ref={containerRef} className="flex items-center justify-center flex-col w-full h-full">
-      <h1 className="text-6xl fixed top-16">Konva.js</h1>
-      <Button>Click me</Button>
+    <div className="flex items-center justify-center flex-col w-full h-full">
+      <div className="fixed top-16 z-10">
+        <Button onClick={handleNewRect}>New Rect</Button>
+      </div>
+      <div className="w-full h-full" ref={containerRef}></div>
     </div>
   );
 }
