921739a2c58f4e7578741a94843c7d9933e11a14
Zhanghao <yukiniro@hotmail.com>
2025-01-09 09:24:04 +0800

    chore: update anime-beta dependency to version 4.0.0-rc.2 and enhance App component with new animation features

--- animejs-v4-demo/src/App.tsx
+++ animejs-v4-demo/src/App.tsx
@@ -4,6 +4,8 @@ import { animate } from "@juliangarnierorg/anime-beta";
 
 function App() {
   const logoRef = useRef<HTMLImageElement>(null);
+  const valueDomRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<{ value: number }>({ value: 0 });
 
   useEffect(() => {
     if (!logoRef.current) return;
@@ -13,12 +15,23 @@ function App() {
       autoplay: true,
       duration: 1000,
     });
+
+    animate(targetRef.current, {
+      value: [100, 500],
+      loop: true,
+      autoplay: false,
+      duration: 1000,
+      onRender: () => {
+        valueDomRef!.current!.innerText = targetRef.current.value.toString();
+      },
+    });
   }, []);
 
   return (
     <div className="container mx-auto h-screen flex flex-col justify-center items-center">
       <h1 className="text-6xl pb-32">Animejs-v4 Demo</h1>
       <img ref={logoRef} src={reactLogo} className="w-36 h-36" alt="react logo" />
+      <div ref={valueDomRef}></div>
     </div>
   );
 }
