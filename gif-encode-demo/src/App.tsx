0a3d523e8774ecc620247f9ebacec8e53049e65e
Zhanghao <yukiniro@hotmail.com>
2025-03-05 15:14:03 +0800

    feat: 添加 gifshot 库并实现另一种 GIF 导出方法

--- gif-encode-demo/src/App.tsx
+++ gif-encode-demo/src/App.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRef } from "react";
 import { GIFEncoder, quantize, applyPalette } from "gifenc";
+// import { AnimatedGIF, defaultOptions } from "gifshot";
+import AnimatedGIF from "./lib/AnimatedGIF";
 
 function App() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -21,7 +23,7 @@ function App() {
     if (!video) return;
     const canvas = document.createElement("canvas");
     const gif = new GIFEncoder();
-    const preFrameTime = 1 / 30;
+    const preFrameTime = 1 / 10;
     const start = 2;
     const end = 3;
     let curTime = start;
@@ -56,12 +58,78 @@ function App() {
     a.click();
   };
 
+  const handleExportWithGifshot = async () => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const images: ImageData[] = [];
+
+    const canvas = document.createElement("canvas");
+    const preFrameTime = 1 / 10;
+    const start = 2;
+    const end = 6;
+    let curTime = start;
+    while (curTime <= end) {
+      await new Promise(resolve => {
+        const fn = () => {
+          video.onseeked = null;
+          resolve(null);
+        };
+        video.onseeked = fn;
+        video.currentTime = curTime;
+      });
+      const ctx = canvas.getContext("2d", { willReadFrequently: true });
+      if (!ctx) return;
+      canvas.width = video.videoWidth / 4;
+      canvas.height = video.videoHeight / 4;
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      curTime += preFrameTime;
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      images.push(imageData);
+    }
+
+    const gif = new AnimatedGIF({
+      // ...defaultOptions,
+      gifWidth: video.videoWidth / 4,
+      gifHeight: video.videoHeight / 4,
+      images: images,
+      numWorkers: 4,
+      frameDuration: 1,
+      numFrames: images.length,
+      sampleInterval: 100,
+      progressCallback: () => {
+        console.log("progressCallback");
+      },
+    });
+
+    images.forEach(imageData => {
+      gif.addFrameImageData(imageData, {
+        delay: Math.round(preFrameTime * 10),
+      });
+    });
+
+    const blob = await new Promise(resolve => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      gif.getBase64GIF(blob => {
+        resolve(blob);
+      });
+    });
+
+    const url = URL.createObjectURL(blob as Blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "output.gif";
+    a.click();
+  };
+
   return (
     <div className="flex items-center justify-center flex-col w-full h-full">
       <h1 className="text-6xl mb-32">Gif Encode Demo</h1>
       <div className="flex items-center gap-2">
         <Input type="file" accept="video/webm" onChange={handleFileChange} />
-        <Button onClick={handleExport}>Export</Button>
+        <Button onClick={handleExport}>export with gifenc</Button>
+        <Button onClick={handleExportWithGifshot}>export with gifshot</Button>
       </div>
       <video className="w-[640px]" ref={videoRef} />
     </div>
